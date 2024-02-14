6238515b3fc8d846d6c638b27be9c9c0708e0713
CarBe03 <carolopintag2003@hotmail.com>
2024-02-14 17:00:41 -0500

    inicio

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
-<?php
-require 'config/conex.php';
-$db =new Database();
-$con = $db->conectar();
-$sql = $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo =1");
-$sql->execute();
-$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-?>
-<!DOCTYPE html>
 <html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda Online</title>
+    <title>Tienda PepeBotella</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -55,8 +46,8 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                     <div class="card shadow-sm">
                         <img src="https://i.ibb.co/XCNWQHD/principal.jpg">
                         <div class="card-body">
-                            <h5 class="card-title">Zapatos color cafe</h5>
-                            <p class="card-text">$ 599.00</p>
+                            <h5 class="card-title">Cerveza</h5>
+                            <p class="card-text">$2,50</p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                     <a href="#" class="btn btn-primary">Detalles</a>
